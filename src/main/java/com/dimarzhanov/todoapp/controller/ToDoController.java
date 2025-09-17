f7418b407a1d611ea5db1b042b55cf7bf366625c
mr-arzh <asiris.joy@icloud.com>
2025-09-17 11:43:16 +0300

    feat: show data from controller on html

--- src/main/java/com/dimarzhanov/todoapp/controller/ToDoController.java
+++ src/main/java/com/dimarzhanov/todoapp/controller/ToDoController.java
@@ -1,13 +1,15 @@
 package com.dimarzhanov.todoapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ToDoController {
 
     @GetMapping
-    public String index() { //стартовая страница
+    public String index(Model model) {
+        model.addAttribute("data", "Hello");//стартовая страница
         return "index";
     }
 }
