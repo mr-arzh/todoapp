d228e5522aa6fbd957241fc08148a33499206b1c
mr-arzh <asiris.joy@icloud.com>
2025-09-19 13:28:05 +0300

    fix: TodoItem fixed, created CSS visualisation

--- src/main/java/com/dimarzhanov/todoapp/controller/ToDoController.java
+++ src/main/java/com/dimarzhanov/todoapp/controller/ToDoController.java
@@ -2,6 +2,7 @@ package com.dimarzhanov.todoapp.controller;
 
 import com.dimarzhanov.todoapp.model.TodoItem;
 import com.dimarzhanov.todoapp.repositories.TodoItemRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 @Controller
-public class ToDoController {
+public class ToDoController implements CommandLineRunner {
 
     private final TodoItemRepository todoItemRepository;
 
@@ -19,11 +20,21 @@ public class ToDoController {
 
     @GetMapping
     public String index(Model model) {
-        todoItemRepository.save(new TodoItem("Item 1"));
-        todoItemRepository.save(new TodoItem("Item 2"));
+//        todoItemRepository.save(new TodoItem("Item 1"));  при запуске html страницы каждый раз всплывают тестовые айтемы
+//        todoItemRepository.save(new TodoItem("Item 2"));  перенес их вниз в метод ран интерфейса CommandLineRunner
         List<TodoItem> allTodos = todoItemRepository.findAll();
-        model.addAttribute("data", allTodos);
+        model.addAttribute("allTodos", allTodos);
+
+//        for(TodoItem todo : allTodos){
+//            System.out.println(todo);
+//        }
         //model.addAttribute("data", "Hello");//стартовая страница
         return "index";
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        todoItemRepository.save(new TodoItem("Item 1"));
+        todoItemRepository.save(new TodoItem("Item 2"));
     }
 }
